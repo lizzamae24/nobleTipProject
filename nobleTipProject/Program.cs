d7414a688cbafee3adab5645ba882dbc02d2cbcd
Eliseo <Eliseo@LAPTOP-OIA8MLF7>
2023-10-23 08:25:50 +0700

    Add project files.

---
+++ nobleTipProject/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        double totalBill;
+        double liquorCharge;
+        double tipPercentage;
+        double tip;
+
+        Console.Write("Enter total bill: ");
+        totalBill = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter liquor charge: ");
+        liquorCharge = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter tip percentage: ");
+        tipPercentage = double.Parse(Console.ReadLine());
+
+        tip = (totalBill - liquorCharge) * tipPercentage;
+
+        Console.WriteLine($"Tip: {tip}");
+    }
+}
